69da7bc9fcceb085fde454ba022ed682c71ea811
Corbin Walker <cdog10123@gmail.com>
2016-01-20 08:11:20 -0700

    all me

---
+++ 1600-001-corbinwalker/Assets/Scripts/PublicVarandFunc1.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PublicVarandFunc1 : MonoBehaviour 
+{
+	public int  myInt = 2000;
+		
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		myInt = MultiplyByTwo(myInt);
+		Debug.Log (myInt); 
+	}
+
+		int MultiplyByTwo (int number)
+	{
+		int ret;
+		ret = number * 2;
+		return ret;
+	}
+}
---
+++ 1600-001-corbinwalker/Assets/Scripts/VarandFunc.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VarandFunc : MonoBehaviour {
+
+	int myInt =10;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		myInt = MultiplyByTwo(myInt);
+		Debug.Log (myInt); 
+	}
+
+	int MultiplyByTwo (int number)
+	{
+		int ret;
+		ret = number * 2;
+		return ret;
+
+	}
+
+}
