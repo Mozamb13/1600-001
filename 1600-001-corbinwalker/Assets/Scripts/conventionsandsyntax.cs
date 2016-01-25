58818feb1f8418dcac8007a74dfac1f1fb17c165
Corbin Walker <cdog10123@gmail.com>
2016-01-25 08:16:06 -0700

    this was me

---
+++ 1600-001-corbinwalker/Assets/Scripts/conventionsandsyntax.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class conventionsandsyntax : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		//The following line of code is used to track the position on the falling cube. Debug.log();
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+		
+		Debug.Log (transform.position.y);
+
+		if (transform.position.y <= 5f) 
+		{
+			Debug.Log ("I'm going to splatter!");
+		}
+
+	}
+}
