90f0122cb2df4ceca956c2b6c8a41a37e4681505
Corbin Walker <cdog10123@gmail.com>
2016-02-22 08:47:24 -0700

    finished projects

---
+++ 1600-001-corbinwalker/Assets/Scripts/ScopeandAccessmodifer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScopeandAccessmodifer : MonoBehaviour 
+{
+	public int alpha = 8;
+
+	private int beta = 0;
+	private int gamma = 8; 
+
+	private Anotherclass myOtherclass;
+
+	void Start ()
+	{
+		alpha = 30;
+		myOtherclass = new Anotherclass ();
+		myOtherclass.Digivice (alpha, myOtherclass.agumon); 
+	}
+
+	void Example (int digimon, int pokemon) 
+	{
+		int answer;
+		answer = digimon * pokemon * alpha;
+		Debug.Log (answer);
+	
+	}
+
+	void Update () 
+	{
+		Debug.Log ("Alpha is set to: " + alpha);
+	}
+}
