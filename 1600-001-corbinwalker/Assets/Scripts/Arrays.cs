d93cf923fe29dd050f573cfb0880b68fef750ae7
Corbin Walker <cdog10123@gmail.com>
2016-02-29 09:04:07 -0700

    finished this assignment today due on the 4th

---
+++ 1600-001-corbinwalker/Assets/Scripts/Arrays.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrays : MonoBehaviour 
+{
+	public GameObject[] Troopers;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Troopers = GameObject.FindGameObjectsWithTag ("Player");
+
+		for (int i = 0; i < Troopers.Length; i++) {
+			Debug.Log ("Trooper " + i + " is named" + Troopers [i].name);
+		}
+	}
+}
