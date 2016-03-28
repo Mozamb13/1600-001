f8be0122315cc048fe2a85aa62a2e7069c919b2e
Corbin Walker <cdog10123@gmail.com>
2016-03-28 09:13:53 -0600

    did today on 03/28/16

---
+++ 1600-001-corbinwalker/Assets/Scripts/Enumeration.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enumeration : MonoBehaviour {
+
+	enum Direction{Up, Right, Down, Left};
+
+	void Start()
+	{
+		Direction myDirection;
+
+		myDirection = Direction.Up;
+	}
+	Direction ReverseDirection (Direction dir)
+	{
+		if (dir == Direction.Up)
+			dir = Direction.Down;
+		if (dir == Direction.Down)
+			dir = Direction.Up;
+		if (dir == Direction.Right)
+			dir = Direction.Left;
+		if (dir == Direction.Left)
+			dir = Direction.Right;
+
+		return dir;
+	}
+}
+
